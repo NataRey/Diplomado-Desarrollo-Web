4b005e6ad0ed2ec45df9a9c8d481024cc83669a6
NataRey <lenarero@gmail.com>
2026-04-06 21:12:21 -0500

    metodos y objetos

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Este documento contiene la explciacion de ciclo, bucles con if switch case, for 
 1. se hizo un ejemplo con switch case anidando if 
 2. se hizo un ejemplo de contrar los numero hasta 10 con for, while y do while
 3. metodos array 
+4. metodos
 
---
+++ metodos.js
@@ -0,0 +1,65 @@
+/**Los metodos son funciones que pertenecen a un objeto 
+ * Acciones ejemplo de un carro
+ * 1. Encender el motor
+ * 2. acelerar 
+ * 3. frenar
+ * 4. girar volante 
+ * 5. encender luces
+ * 6. apagar luces
+ * 7. ir en reversa
+ * 8. tocar bocina
+ * 9. limpiar el parabrisas
+ * 10.apagar el motor
+ * 
+ * propiedades o atributos
+ * 1. Color
+ * 2. carroceria
+ * 3. cilindraje
+ * 4. marca
+ * 5. modelo
+ * 6. tipo de combustible
+ * 7. velocidad
+*/
+
+const carro = {
+    marca:'Toyota',
+    modelo:'Corolla',
+    velocidad: 0,
+
+    //metodo para encender el carro
+    encender: function(mensaje){
+        this.mensaje="";
+        console.log(mensaje);
+    },
+
+    //metodo para acelerar el carro
+    acelerar: function(cantidad){
+        this.velocidad += cantidad;
+        console.log(`El carro acelera, su velocidad actual es ${this.velocidad}km/h.`);
+    },
+
+    //metodo para frenar el carro
+    frenar: function(cantidad){
+        //verificar si la velocidad actual ya es 0 
+        if(this.velocidad === 0){
+            console.log("El carro esta detenido, no se puede frenar mas");
+            return
+        }
+        //si la velocidad no es 0 podemos frenar
+        this.velocidad -= cantidad; 
+        if(this.velocidad<0){
+            this.velocidad = 0;
+        }
+        console.log(`El carro frena, su velocidad actual es ${this.velocidad}km/h.`);
+
+    },
+
+
+    // metodo para girar el volante
+    
+
+}
+
+
+
+
